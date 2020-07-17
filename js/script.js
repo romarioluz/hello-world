5d7694ea1da4a2bb07ff4700a5da30a44db33aa9
romarioluz <62477504+romarioluz@users.noreply.github.com>
2020-07-16 22:32:44 -0300

    adicionadas funcionalidades

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ let tarefas = [
 ];
 
 function renderizarTarefas(){
+    //Limpar a listagem de itens antes de renderizar a tela
+    lista.innerHTML = ''
     for(tarefa of tarefas){
         //criar o item da lista
         let itemLista = document.createElement('li')
@@ -31,3 +33,51 @@ function renderizarTarefas(){
 }
 
 renderizarTarefas()
+
+//Adicionar evento de click no botão
+//captar o valor do input
+btn.onclick = function() {
+    let novaTarefa = input.value;
+
+    if(novaTarefa.length >0){
+
+        //renderizar a tela novamente
+        tarefas.push(novaTarefa);
+        renderizarTarefas();
+    
+        //Limpar o input
+        input.value = ''
+        
+    }else{
+        let card = document.querySelector('.card')
+        let span = document.createElement('span')
+        span.setAttribute('class', 'alert alert-warning')
+
+        let msg = document.createTextNode('Você precisa informar a tarefa!')
+        span.appendChild(msg)
+        card.appendChild(span)
+    }
+    
+}
+
+function removerSpans(){
+    let spans = document.querySelectorAll('.span')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
